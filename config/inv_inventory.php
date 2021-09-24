ee945106300b59ec54687b97ebb5afc2636f756c
Wei Shen <weishenemail@gmail.com>
2021-09-24 23:29:41 +0800

    Add Config File, View Folders

---
+++ config/inv_inventory.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Configuration For The Inventory System
+    |--------------------------------------------------------------------------
+    |
+    | This values are used by the inventory system
+    |
+    | Updated: 2021-09-24
+    */
+
+    'inv_name' => 'Inventory System',
+
+];
