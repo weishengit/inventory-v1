e5c6d61a85802463839057ea13a14e1e31060e43
Wei Shen <weishenemail@gmail.com>
2021-10-26 12:11:41 +0800

    Fix user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,23 +15,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        if (User::count() <= 0) {
-            User::firstOrCreate([
-                'id' => 1,
-                'role_id' => 1,
-                'name' => 'lorenz',
-                'email' => 'lorenz@example.com',
-                'password' => Hash::make('123')
-            ]);
+        User::firstOrCreate([
+            'role_id' => 1,
+            'name' => 'lorenz',
+            'email' => 'lorenz@example.com',
+            'password' => Hash::make('123')
+        ]);
 
-            User::firstOrCreate([
-                'id' => 2,
-                'role_id' => 2,
-                'name' => 'loraine',
-                'email' => 'loraine@example.com',
-                'password' => Hash::make('123')
-            ]);
-        }
+        User::firstOrCreate([
+            'role_id' => 2,
+            'name' => 'loraine',
+            'email' => 'loraine@example.com',
+            'password' => Hash::make('123')
+        ]);
 
         User::factory(10)->create();
     }
